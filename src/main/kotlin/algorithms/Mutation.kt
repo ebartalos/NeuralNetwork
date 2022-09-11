528dfb8d95a4384819db7e4a8b7380357f5b58e5
erikbartalos <no>
2022-09-11 18:26:43 +0200

    Add mutation

---
+++ src/main/kotlin/algorithms/Mutation.kt
@@ -0,0 +1,15 @@
+package algorithms
+
+import Network
+import kotlin.random.Random
+
+class Mutation(private val network: Network) {
+
+    fun mutate() {
+        for (layer in network.layers){
+            for (connection in layer.outgoingConnections){
+                connection.weight = connection.weight *  Random.nextDouble(0.85, 1.15)
+            }
+        }
+    }
+}
