fb6d288e80094609f6abba3ac0aef6f74275fb36
ebartalos <bartalos@protonmail.com>
2024-02-14 16:50:09 +0100

    Cleanup

--- src/main/kotlin/ai/algorithms/Genetics.kt
+++ src/main/kotlin/ai/algorithms/Genetics.kt
@@ -10,7 +10,6 @@ import kotlin.random.Random
  * @param networks ordered list of networks by fitness (starting with the best fitness)
  */
 class Genetics(private val networks: List<Network>) {
-
     /**
      * Take two best networks and breed them.
      * Top 2 networks are passed down.
@@ -45,7 +44,7 @@ class Genetics(private val networks: List<Network>) {
 
         if (mutate) {
             for (network in networks.subList(passDown, lastFifth)) {
-                mutate(network, MUTATION_RANGE_FROM, MUTATION_RANGE_TO, MUTATION_PERCENT_CHANCE)
+                mutate(network)
             }
         }
 
@@ -66,16 +65,16 @@ class Genetics(private val networks: List<Network>) {
      * @param to higher index of mutation range
      * @param chance percentage chance to mutate
      */
-    private fun mutate(network: Network, from: Double, to: Double, chance: Int) {
+    private fun mutate(network: Network) {
         for (layer in network.layers) {
             for (connection in layer.outgoingConnections) {
-                if (Random.nextInt() % 100 < chance) {
+                if (Random.nextInt() % 100 < MUTATION_PERCENT_CHANCE) {
                     if (connection.weight == 0.0) {
                         connection.weight = Random.nextDouble(-0.01, 0.01)
                     } else if (Random.nextBoolean()) {
-                        connection.weight += (connection.weight * Random.nextDouble(from, to)) - connection.weight
+                        connection.weight += (connection.weight * Random.nextDouble(MUTATION_RANGE_FROM, MUTATION_RANGE_TO)) - connection.weight
                     } else {
-                        connection.weight -= (connection.weight * Random.nextDouble(from, to)) - connection.weight
+                        connection.weight -= (connection.weight * Random.nextDouble(MUTATION_RANGE_FROM, MUTATION_RANGE_TO)) - connection.weight
                     }
                 }
             }
