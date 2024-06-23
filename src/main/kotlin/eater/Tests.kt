f4e0c0df5aa710ae9e3d6f962f1665bc2adc9cd1
ebartalos <bartalos@protonmail.com>
2024-06-23 12:31:05 +0200

    Clean code

--- src/main/kotlin/eater/Tests.kt
+++ src/main/kotlin/eater/Tests.kt
@@ -13,22 +13,24 @@ object Tests {
 
         println("Number of connections before pruning: ${network.getConnections()}")
 
+        val runs = 10
+
         val timeTakenBeforePruning = measureTime {
-            for (i in 0..1) {
+            for (i in 0..runs) {
                 Game((Eater(network)), playgroundSize).play(Constants.MAX_FITNESS, useGUI = false)
             }
         }
-        println("Time before pruning: ${timeTakenBeforePruning.inWholeMilliseconds}ms")
+        println("Time before pruning: ${timeTakenBeforePruning.inWholeSeconds}s")
 
         network.prune()
 
         println("Number of connections after pruning: ${network.getConnections()}")
         val timeTakenAfterPruning = measureTime {
-            for (i in 0..1) {
+            for (i in 0..runs) {
                 Game((Eater(network)), playgroundSize).play(Constants.MAX_FITNESS, useGUI = false)
             }
         }
-        println("Time after pruning: ${timeTakenAfterPruning.inWholeMilliseconds}ms")
-        println("Time saved: ${timeTakenBeforePruning.minus(timeTakenAfterPruning)}ms")
+        println("Time after pruning: ${timeTakenAfterPruning.inWholeSeconds}s with threshold ${Constants.PRUNING_THRESHOLD}")
+        println("Time saved: ${(timeTakenBeforePruning.minus(timeTakenAfterPruning)).inWholeSeconds}s for $runs runs")
     }
 }
